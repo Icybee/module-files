f3314d84ecfc2fb33709d0973d95c5432ad25dd4
Olivier Laviale <contact@weirdog.com>
2013-04-11 17:27:43 +0200

    The Files module

---
+++ lib/activerecords/file.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the Icybee package.
+*
+* (c) Olivier Laviale <olivier.laviale@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Icybee\Modules\Files;
+
+/**
+ * Representation of a managed file.
+ *
+ * @property-read string $extension The file extension.
+ */
+class File extends \Icybee\Modules\Nodes\Node
+{
+	const PATH = 'path';
+	const MIME = 'mime';
+	const SIZE = 'size';
+	const DESCRIPTION = 'description';
+
+	/**
+	 * Path of the file, relative to the DOCUMENT_ROOT.
+	 *
+	 * @var string
+	 */
+	public $path;
+
+	/**
+	 * MIME type of the file.
+	 *
+	 * @var string
+	 */
+	public $mime;
+
+	/**
+	 * Size of the file.
+	 *
+	 * @var int
+	 */
+	public $size;
+
+	/**
+	 * Description of the file.
+	 *
+	 * @var string
+	 */
+	public $description;
+
+	/**
+	 * Defaults the model to "files".
+	 */
+	public function __construct($model='files')
+	{
+		parent::__construct($model);
+	}
+
+	/**
+	 * Returns the extension of the file.
+	 *
+	 * Note: The dot is included e.g. ".zip".
+	 *
+	 * @return string
+	 */
+	protected function volatile_get_extension()
+	{
+		return '.' . pathinfo($this->path, PATHINFO_EXTENSION);
+	}
+
+	public function url($type='view')
+	{
+		if ($type == 'download')
+		{
+			return ($this->siteid ? $this->site->path : '') . '/api/' . $this->constructor . '/' . $this->nid . '/download';
+		}
+
+		return parent::url($type);
+	}
+}
